3440f7b051c727fd2d5cbd06e39341fed0540db6
Aeron01 <one.soulreaper@gmail.com>
2021-03-25 14:05:44 +0100

    1st commit

--- echo.js
+++ echo.js
@@ -1,1 +1,8 @@
-process
+const chalk = require('chalk');
+
+if (process.argv.length !== 7) {
+  console.log(chalk.redBright('Usage: node echo.js string1 string2 string3 string4 string5'))
+  process.exit(1)
+}
+
+console.log(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6])
