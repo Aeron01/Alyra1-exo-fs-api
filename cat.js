4882bff3dc9095baa8c88be01c8fc89b9df773a0
Aeron01 <one.soulreaper@gmail.com>
2021-03-31 16:52:46 +0200

    updated

--- cat.js
+++ cat.js
@@ -5,17 +5,18 @@ if (/*process.argv.length > process.argv[process.argv.length - 1] || */process.a
   console.log(chalk.red('Usage: node cat.js file.ext'))
   process.exit(1)
 }
-if (!stats.isFile()) {
-  console.log(chalk.red(`Error: ${process.argv[i]} is not a file`))
-  process.exit(1)
-}
+
 for (let i = 2; i < process.argv.length; i++) {
   if (!fs.existsSync(process.argv[i])) {
-    console.log(chalk.red(`Error: ${process.argv[i]} does not exist`))
-    process.exit(1)
+    console.log(chalk.yellow(`Error: ${process.argv[i]} does not exist`))
+    continue
   }
 
   const stats = fs.statSync(process.argv[i])
+  if (!stats.isFile()) {
+    console.log(chalk.yellow(`Error: ${process.argv[i]} is not a file`))
+    continue
+  }
 
   let file = fs.readFileSync(process.argv[i], 'utf-8')
   file += `\n`
--- tail2.js
+++ tail2.js
@@ -1,45 +1,54 @@
+//--Chargement des modules dont on a besoin
 const chalk = require('chalk');
 const fs = require('fs')
+//Definission des paramettre par defaut
 let nbLines = 10
 let indexFile = 2
-
+//Verification des argument entrer en ligne de commande
 if (process.argv.length < 3 || process.argv.length > 5) {
   console.log(chalk.red('Usage: node tail.js [option: -n Number (default 10)] file.txt -The file must contain at least 10 lines!'))
   process.exit(1)
 }
-
+//Verification si l'option '-n' est présente ou non dans les arguments en ligne de commande
+//et vérification si l'argument après l'option est bien un nombre
 if (process.argv[2] === '-n') {
   if (isNaN(process.argv[3])) {
     console.log(chalk.redBright(`Error: ${process.argv[3]} is not a number.`))
     console.log(chalk.red('Usage: node tail.js [option: -n Number (default 10)] file.txt -The file must contain at least 10 lines!'))
     process.exit(1)
   }
-  nbLines = Number(process.argv[3])
+  nbLines = Math.abs(Number(process.argv[3]))
   indexFile += 2
 }
-console.log(indexFile)
-console.log(nbLines)
+
+//Vérification si le fichier existe bien
+
 if (!fs.existsSync(process.argv[indexFile])) {
   console.log(chalk.red(`Error: ${process.argv[indexFile]} does not exist`))
   process.exit(1)
 }
 
-const stats = fs.statSync(process.argv[indexFile])
+//Vérification si le fichier n'est pas un dossier
 
+const stats = fs.statSync(process.argv[indexFile])
 if (!stats.isFile()) {
   console.log(chalk.red(`Error: ${process.argv[indexFile]} is not a file`))
   process.exit(1)
 }
 
-const txt = fs.readFileSync(process.argv[indexFile], 'utf-8')
-let nbFileLine = txt.split('\n')
+//Vérification si le fichier a bien un nombre supérieur au nombre de ligne souhaiter
 
-if (nbFileLine.length < nbLines) {
-  console.log(chalk.redBright(`Error: The file is under ${nbLines} line. The File ${process.argv[indexFile]} contains ${nbFileLine.length} lines.`))
-  process.exit(1)
+const txt = fs.readFileSync(process.argv[indexFile], 'utf-8')
+let tabtxt = txt.split('\n')
+let numFileLine = tabtxt.length
+if (numFileLine < nbLines) {
+  console.log(chalk.yellowBright(`The file is under ${nbLines} line. The File ${process.argv[indexFile]} contains ${numFileLine} lines.`))
+  nbLines = numFileLine
 }
 
+//Traitement de l'affichage de la demande
+
 console.log(chalk.green(`Here are the last ${nbLines} lines of the file named ${process.argv[indexFile]}.`))
-for (let i = nbFileLine.length - nbLines; i < nbFileLine.length; i++) {
-  console.log(nbFileLine[i])
+for (let i = numFileLine - nbLines; i < numFileLine; i++) {
+  console.log(tabtxt[i])
 }
