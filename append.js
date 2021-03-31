f3c6cf7daeff04bf1a4349fcc19fe3225054a826
Aeron01 <one.soulreaper@gmail.com>
2021-03-31 11:21:52 +0200

    up

--- append.js
+++ append.js
@@ -1,13 +1,16 @@
 const chalk = require('chalk');
 const fs = require('fs')
 let contFil = ''
-if (process.argv.length > process.argv[process.argv.length - 1] || process.argv.length < 5) {
-  console.log(chalk.red('Usage min: node append.js file.ext file1.ext file2.ext dst.txt'))
+if (/*process.argv.length > process.argv[process.argv.length - 1] ||*/ process.argv.length < 5) {
+  console.log(chalk.red('Usage min: node append.js file.ext file1.ext dst.txt'))
   console.log(chalk.red('Usage max(right now): node append.js file.ext file1.ext file2.ext file3.ext file4.ext file5.ext dst.txt'))
   process.exit(1)
 }
 
-
+if (fs.existsSync(process.argv[process.argv.length - 1])) {
+  console.log(chalk.red(`Error: ${process.argv[process.argv.length - 1]} exist`))
+  process.exit(1)
+}
 for (let i = 2; i < process.argv.length - 1; i++) {
   if (!fs.existsSync(process.argv[i])) {
     console.log(chalk.red(`Error: ${process.argv[i]} does not exist`))
@@ -15,12 +18,14 @@ for (let i = 2; i < process.argv.length - 1; i++) {
   }
 
   const stats = fs.statSync(process.argv[i])
+
   if (!stats.isFile()) {
     console.log(chalk.red(`Error: ${process.argv[i]} is not a file`))
     process.exit(1)
   }
 
   let fileCont = fs.readFileSync(process.argv[i], 'utf-8')
+  //fileCont += '\n'
 
   fs.appendFileSync(`${process.argv[process.argv.length - 1]}`, `${fileCont}\n`, 'utf-8')
 
--- cat.js
+++ cat.js
@@ -1,11 +1,14 @@
 const chalk = require('chalk');
 const fs = require('fs')
 
-if (process.argv.length > process.argv[process.argv.length - 1] || process.argv.length < 3) {
+if (/*process.argv.length > process.argv[process.argv.length - 1] || */process.argv.length < 3) {
   console.log(chalk.red('Usage: node cat.js file.ext'))
   process.exit(1)
 }
-
+if (!stats.isFile()) {
+  console.log(chalk.red(`Error: ${process.argv[i]} is not a file`))
+  process.exit(1)
+}
 for (let i = 2; i < process.argv.length; i++) {
   if (!fs.existsSync(process.argv[i])) {
     console.log(chalk.red(`Error: ${process.argv[i]} does not exist`))
@@ -13,12 +16,8 @@ for (let i = 2; i < process.argv.length; i++) {
   }
 
   const stats = fs.statSync(process.argv[i])
-  if (!stats.isFile()) {
-    console.log(chalk.red(`Error: ${process.argv[i]} is not a file`))
-    process.exit(1)
-  }
 
   let file = fs.readFileSync(process.argv[i], 'utf-8')
-
+  file += `\n`
   console.log(file)
 }
