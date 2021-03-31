26c6c77b47986e0d9f8ffcb44c6529baf90f2533
Aeron01 <one.soulreaper@gmail.com>
2021-03-31 15:34:04 +0200

    tail2.js update

--- tail2.js
+++ tail2.js
@@ -1,44 +1,45 @@
 const chalk = require('chalk');
 const fs = require('fs')
+let nbLines = 10
+let indexFile = 2
 
-if (process.argv.length < 3 || process.argv.length === 4 || process.argv.length > 5) {
+if (process.argv.length < 3 || process.argv.length > 5) {
   console.log(chalk.red('Usage: node tail.js [option: -n Number (default 10)] file.txt -The file must contain at least 10 lines!'))
   process.exit(1)
 }
-let line = process.argv[2]
-if (process.argv[4] === undefined) {
-  process.argv[4] = line
+
+if (process.argv[2] === '-n') {
+  if (isNaN(process.argv[3])) {
+    console.log(chalk.redBright(`Error: ${process.argv[3]} is not a number.`))
+    console.log(chalk.red('Usage: node tail.js [option: -n Number (default 10)] file.txt -The file must contain at least 10 lines!'))
+    process.exit(1)
+  }
+  nbLines = Number(process.argv[3])
+  indexFile += 2
 }
-console.log(line)
-if (!fs.existsSync(process.argv[4])) {
-  console.log(chalk.red(`Error: ${process.argv[4]} does not exist`))
-  process.exit(1)
-}
-if (isNaN(process.argv[3])) {
-  (console.log(chalk.redBright(`Error: ${process.argv[3]} is not a number.`)))
+console.log(indexFile)
+console.log(nbLines)
+if (!fs.existsSync(process.argv[indexFile])) {
+  console.log(chalk.red(`Error: ${process.argv[indexFile]} does not exist`))
   process.exit(1)
 }
 
-const stats = fs.statSync(process.argv[4])
+const stats = fs.statSync(process.argv[indexFile])
 
 if (!stats.isFile()) {
-  console.log(chalk.red(`Error: ${process.argv[4]} is not a file`))
-  process.exit(1)
-}
-let nbDspLine = process.argv[3]
-if (process.argv[2] !== '-n') {
-  nbDspLine = 10
-}
-
-const txt = fs.readFileSync(process.argv[4], 'utf-8')
-let nbline = txt.split('\n')
-
-if (nbline.length < nbDspLine) {
-  console.log(chalk.redBright(`Error: The file is under 10 line. ${process.argv[4]} contains ${nbline.length} lines.`))
+  console.log(chalk.red(`Error: ${process.argv[indexFile]} is not a file`))
   process.exit(1)
 }
 
-console.log(chalk.green(`Here are the last ${nbDspLine} lines of the file named ${process.argv[4]}.`))
-for (let i = nbline.length - nbDspLine; i < nbline.length; i++) {
-  console.log(nbline[i])
+const txt = fs.readFileSync(process.argv[indexFile], 'utf-8')
+let nbFileLine = txt.split('\n')
+
+if (nbFileLine.length < nbLines) {
+  console.log(chalk.redBright(`Error: The file is under ${nbLines} line. The File ${process.argv[indexFile]} contains ${nbFileLine.length} lines.`))
+  process.exit(1)
+}
+
+console.log(chalk.green(`Here are the last ${nbLines} lines of the file named ${process.argv[indexFile]}.`))
+for (let i = nbFileLine.length - nbLines; i < nbFileLine.length; i++) {
+  console.log(nbFileLine[i])
 }
